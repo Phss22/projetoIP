089d14ae31e2a6d7911308f16505ea1e0da619f5
Milla Rwana de araújo Silva <mras3@cin.ufpe.br>
2024-07-25 17:02:07 -0300

    Create config.py
    módulo que contém todas as configurações iniciais.

---
+++ config.py
@@ -0,0 +1,21 @@
+# config.py
+import pygame
+
+# iniciar pygame
+pygame.init()
+
+# nome do jogo
+pygame.display.set_caption("Jogo Snake Python")
+
+# configurações da tela
+LARGURA, ALTURA = 800, 600
+TAMANHO_QUADRADO = 10
+VELOCIDADE_JOGO = 15
+
+# cores
+PRETA = (0, 0, 0)
+BRANCA = (255, 255, 255)
+VERMELHA = (255, 0, 0)
+VERDE = (0, 255, 0)
+AZUL = (0, 0, 255)
+AMARELO = (255, 255, 0)
